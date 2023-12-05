35bd2b6b82575a44b9056c29085498b66a591735
miji <mijilee0413@sookmyung.ac.kr>
2023-12-05 15:54:03 +0900

    SMMarble
    2023.12.05

---
+++ smm_common.h
@@ -0,0 +1,21 @@
+//
+//  smm_common.h
+//  SMMarble
+//
+//  Created by MIJI LEE on 2023/12/04.
+//
+
+#ifndef smm_common_h
+#define smm_common_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_CHARNAME                200
+
+#define GRADUATE_CREDIT             30
+#define MAX_DIE                     6
+#define MAX_PLAYER                  100
+
+
+#endif /* smm_common_h */
---
+++ smm_object.c
@@ -0,0 +1,77 @@
+//
+//  smm_node.c
+//  SMMarble
+//
+//  Created by MIJI LEE on 2023/12/04.
+//
+
+#include "smm_common.h"
+#include "smm_object.h"
+#include <string.h>
+
+#define MAX_NODETYPE    7
+#define MAX_GRADE       9
+#define MAX_NODE        100
+
+
+static char smmNodeName[SMMNODE_TYPE_MAX][MAX_CHARNAME] = {
+       "����",
+       "�Ĵ�",
+       "�����",
+       "��",
+       "����Ƿ��̵�",
+       "��������",
+       "�����ð�"
+};
+
+char* smmObj_getTypeName(int type)
+{
+      return (char*)smmNodeName[type];
+}
+
+//1. ����ü ���� ����
+typedef struct smmObject {
+       char name[MAX_CHARNAME];
+       int type;
+       int credit;
+       int energy;
+} smmObject_t;
+
+//2. ����ü �迭 ���� ���� 
+static smmObject_t smm_node[MAX_NODE];
+
+static int smmObj_noNode = 0;
+
+//3. ���� �Լ� ���� 
+//object generation
+void smmObj_genNode(char* name, int type, int credit, int energy)
+{    
+    strcpy(smm_node[smmObj_noNode].name, name);
+    smm_node[smmObj_noNode].type = type;
+    smm_node[smmObj_noNode].credit = credit;
+    smm_node[smmObj_noNode].energy = energy;
+    
+    smmObj_noNode++;
+}
+
+//3. ���� �Լ� ���� 
+char* smmObj_getNodeName(int node_nr)
+{
+    return smm_node[node_nr].name;
+}
+
+//3. ���� �Լ� ���� 
+int smmObj_getNodeType(int node_nr)
+{
+    return smm_node[node_nr].type;
+}
+
+int smmObj_getNodeCredit(int node_nr)
+{
+    return smm_node[node_nr].credit;
+}
+
+int smmObj_getNodeEnergy(int node_nr)
+{
+    return smm_node[node_nr].energy;
+}
